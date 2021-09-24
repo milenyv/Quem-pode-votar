b2c80452ea04682d017bfccc53c94d061ea8f0b5
mileny <82714273+milenyv@users.noreply.github.com>
2021-09-24 12:16:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de dados
+            int idade = 0;
+
+            Console.WriteLine("Qual sua idade? ");
+            bool sit1 = idade >= 0 && idade <= 15; // não vota
+            bool sit2 = idade >= 16 && idade <= 17; // opcional
+            bool sit3 = idade >= 18 && idade < 70; // obrigatorio 
+            bool sit4 = idade > 70; //opcional
+
+            //resultados
+            Console.WriteLine($"");
+
+
+
+
+
+
+        }
+    }
+}
